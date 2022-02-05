3276c2076b745f69559c6baefe136a320f8d7276
Eatableninjago11 <83182611+Eatableninjago11@users.noreply.github.com>
2022-02-05 10:57:23 -0600

    Add files via upload

---
+++ save.py
@@ -0,0 +1,37 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+import csv
+import statistics
+import numpy as np
+
+df = pd.read_csv("savings_data_final.csv")
+
+fig = px.scatter(df, y="quant_saved", color="rem_any")
+fig.show()
+
+
+with open('savings_data_final.csv', newline="") as f:
+  reader = csv.reader(f)
+  savings_data = list(reader)
+
+savings_data.pop(0)
+
+#Finding total number of people and number of people who were reminded
+total_entries = len(savings_data)
+total_people_given_reminder = 0
+for data in savings_data:
+  if int(data[3]) == 1:
+    total_people_given_reminder += 1
+
+import plotly.graph_objects as go
+
+
+all_savings = []
+for data in savings_data:
+  all_savings.append(float(data[0]))
+
+print(f"Mean of savings - {statistics.mean(all_savings)}")
+print(f"Median of savings - {statistics.median(all_savings)}")
+print(f"Mode of savings - {statistics.mode(all_savings)}")
